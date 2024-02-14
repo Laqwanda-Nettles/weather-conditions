efb0c25989327c0f21b58908e0c3e55f9986a79b
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-02-13 22:21:27 -0600

    created html + js files and linked them. Created a class called Weather and a sub class called DayWeather.

---
+++ script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+class Weather {
+  constructor(temperature, precipitation, description) {
+    this.temperature = temperature;
+    this.precipitation = precipitation;
+    this.description = description;
+  }
+  displayConditions() {
+    return `Temperature: ${this.temperature} °F, Precipitation: ${this.precipitation}%, Description: ${this.description}`;
+  }
+}
+
+class DayWeather extends Weather {
+  constructor(temperature, precipitation, description, weekDay, humidity) {
+    super(temperature, precipitation, description);
+    this.weekDay = weekDay;
+    this.humidity = humidity;
+  }
+  alert() {
+    if (this.temperature >= 90) {
+      return "Excessive Heat Warning: Please limit your time outdoors.";
+    } else {
+      return "There are no warnings today.";
+    }
+  }
+  displayConditions() {
+    return `Today is ${this.weekDay}. Temprature: ${this.temperature} °F, Precipitation: ${this.precipitation}%, Humidity: ${this.humidity}%, Description: ${this.description}`;
+  }
+}
+
+const todaysWeather = new Weather(64, 0, "Mostly cloudy");
+const wednesdaysWeather = new DayWeather(
+  64,
+  0,
+  "Mostly cloudy",
+  "Wednesday",
+  62
+);
+
+todaysWeather.displayConditions();
+
+wednesdaysWeather.displayConditions();
+wednesdaysWeather.alert();
